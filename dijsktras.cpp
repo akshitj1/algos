d7c1cfa3ef2abf8a0665916c51d52337c485570f
Akshit Jain <akshit.jain@zettata.com>
2015-09-16 16:57:16 +0530

    dijskarts backtracking added

--- dijsktras.cpp
+++ dijsktras.cpp
@@ -32,17 +32,20 @@ const int maxd=maxn*maxw;
 
 vector<ii> gr[maxn];
 int minDist[maxn];
+int prev[maxn];
 bool vis[maxn];
 
-void djInit(int n, int (&minDist)[maxn]){
-	forn(i,n)
+void djInit(int n, int (&minDist)[maxn], int (&prev)[maxn]){
+	forn(i,n){
 		minDist[i]=maxd;
+		prev[i]=-1;
+	}
 	clr(vis);
 }
 
-void dijsktras(int s, int n,const vector<ii> (&gr)[maxn], int (&minDist)[maxn]){
+void dijsktras(int s, int n,const vector<ii> (&gr)[maxn], int (&minDist)[maxn], int (&prev)[maxn]){
+	djInit(n, minDist, prev);
 	minDist[s]=0;
-	djInit(n, minDist);
 	priority_queue<ii, vector<ii>, greater<ii> > pq;
 	pq.push(ii(0, s));
 	while(!pq.empty()){
@@ -50,11 +53,14 @@ void dijsktras(int s, int n,const vector<ii> (&gr)[maxn], int (&minDist)[maxn]){
 		pq.pop();
 		int u=top.second;
 		if(vis[u]) continue;
-		int d=top.first;
-		if(minDist[u]>d)
-			minDist[u]=d;
 		forn(i, gr[u].size()){
-			pq.push(ii(minDist[u]+gr[u][i].second, gr[u][i].first));
+			int v=gr[u][i].first;
+			int alt= minDist[u]+gr[u][i].second;
+			if(minDist[v]>alt){
+				minDist[v]=alt;
+				prev[v]=u;
+			}
+			pq.push(ii(minDist[v], v));
 		}
 		vis[u]=true;
 	}
@@ -70,7 +76,7 @@ int main(){
 		gr[u].pb(ii(v,d));
 		gr[v].pb(ii(u,d));
 	}
-	dijsktras(s, n, gr, minDist);
+	dijsktras(s, n, gr, minDist, prev);
 	forn(i, n)
-		cout<<s+1<<","<<i+1<<" : "<<minDist[i]<<endl;
+		cout<<s+1<<"::"<<prev[i]+1<<" -> "<<i+1<<" : "<<minDist[i]<<endl;
 }
